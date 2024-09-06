294960a1db7086777ceef44a7d36e571c2c99820
rajeshwar38anand <rajeshwar38anand@outlook.com>
2024-09-06 15:53:16 +0530

    mfe-app of microapp

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, 'tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "mfe1",
+    publicPath: "auto",
+    scriptType: 'text/javascript',  // Use 'module' for modern JS module behavior
+  },
+  optimization: {
+    runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true  // This allows Webpack to output as a module
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+         name: "mfeapp",
+         filename: "remoteEntry.js",
+         exposes: {
+             './Component': './src/app/app.component.ts',
+        },
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
